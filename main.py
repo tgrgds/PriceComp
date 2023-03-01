62e6ebd1dbff92639e3b73c5308d3b1fb9e0e6c7
TylerJayKeys <hello@tylerjay.dev>
2023-03-01 18:05:19 +1100

    Add CORS

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from src.api import api
@@ -9,6 +10,14 @@ from src.prisma import prisma
 app = FastAPI(
   title="PriceComp",
   version="0.0.1"
+)
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"], # TODO: set specific origins!!
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"]
 )
 
 app.include_router(api, prefix="/api")
